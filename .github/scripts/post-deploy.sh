b31347e5427bdd7d0e1e000860e026b8f4852928
amaralTheSage <amaraldesouza9@gmail.com>
2026-01-26 17:36:50 -0300

    .

--- .github/scripts/post-deploy.sh
+++ .github/scripts/post-deploy.sh
@@ -6,7 +6,7 @@ echo "::group::Running post-deploy script..."
 
 echo "::notice::Caching framework..."
 php artisan optimize
-php artisan filament:optimize
+# php artisan filament:optimize
 
 php artisan migrate --force --isolated
 
@@ -18,9 +18,9 @@ echo "::notice::Reset storage link just be sure..."
 php artisan storage:unlink
 php artisan storage:link
 
-echo "::notice::Starts Reverb"
-pkill -f "artisan reverb:start" || true
-nohup php artisan reverb:start --debug > storage/logs/reverb.log 2>&1 &
+# echo "::notice::Starts Reverb"
+# pkill -f "artisan reverb:start" || true
+# nohup php artisan reverb:start --debug > storage/logs/reverb.log 2>&1 &
 
 echo "::endgroup::"
 
