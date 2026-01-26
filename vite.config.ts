f314ac26dd953d3d7bc7ea112f65d6517973e09a
amaralTheSage <amaraldesouza9@gmail.com>
2026-01-26 17:23:23 -0300

    ,

--- vite.config.ts
+++ vite.config.ts
@@ -25,12 +25,4 @@ export default defineConfig({
     esbuild: {
         jsx: 'automatic',
     },
-
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        hmr: {
-            host: 'https://spotlight-uniform-minus-played.trycloudflare.com/',
-        },
-    },
 });
