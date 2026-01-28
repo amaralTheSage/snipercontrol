a4574b2a5c26ad0fa9dceaf916f594cba5206d62
amaralTheSage <amaraldesouza9@gmail.com>
2026-01-28 13:40:44 -0300

    adds cleanup script

---
+++ app/Filament/Widgets/DriverMapWidget.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use EduardoRibeiroDev\FilamentLeaflet\Widgets\MapWidget;
+use EduardoRibeiroDev\FilamentLeaflet\Support\Markers\Marker;
+
+class DriverMapWidget extends MapWidget
+{
+    // Map heading
+    protected static ?string $heading = 'Store Locations';
+
+    // Center coordinates [latitude, longitude]
+    protected static array $mapCenter = [-14.235, -51.9253];
+
+    // Initial zoom level (1-18)
+    protected static int $defaultZoom = 4;
+
+    // Map height in pixels
+    protected static int $mapHeight = 600;
+
+    // Zoom configuration
+    protected static int $maxZoom = 18;
+    protected static int $minZoom = 2;
+
+    protected function getMarkers(): array
+    {
+        return [
+            Marker::make(-23.5505, -46.6333)
+                ->title('São Paulo')
+                ->popupContent('The largest city in Brazil'),
+        ];
+    }
+}
